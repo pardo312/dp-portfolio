4fbce1638794c6fa47bbb17089247dd7490e0b32
Jiufen <pardod312@gmail.com>
2023-12-18 15:41:34 -0500

    Automatic language change based on browser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import { redirect } from 'next/navigation';
+"use client";
+import { redirect } from "next/navigation";
+import getUserLocale from "get-user-locale";
+import { useEffect } from "react";
 
 export default function Home() {
-    redirect('/es');
+  useEffect(() => {
+    let userLocale = getUserLocale().substring(0, 2);
+    console.log("User-locale: " + getUserLocale());
+
+    if (userLocale == "es") redirect("/es");
+    else redirect("/en");
+  }, []);
 }
-
-
