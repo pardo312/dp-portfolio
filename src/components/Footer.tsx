4ab6e51cf1900db06bf47c7c2c931b62cf80c920
Jiufen <pardod312@gmail.com>
2023-12-19 12:32:30 -0500

    Fix footer lang

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,7 @@
+import { LocaleDataContext } from "@/context/LocaleContext";
+import { LocaleEnum } from "@/context/LocaleEnum";
 import { Cormorant_Upright } from "next/font/google";
+import { useContext } from "react";
 
 export const cormorantUpright = Cormorant_Upright({
   weight: ["400", "700"],
@@ -6,14 +9,32 @@ export const cormorantUpright = Cormorant_Upright({
 });
 
 export function Footer() {
+  let localeData = useContext(LocaleDataContext);
   return (
-    <div className={"flex justify-between md:mx-20 my-5 md:my-10 px-5 md:px-10 text-md md:text-2xl " + cormorantUpright.className}>
-      <h1 className="leading-tight text-matrix-light">
-        Creado con <span className="text-matrix-normal">Next.js</span>
-      </h1>
-      <h1 className="leading-tight text-matrix-light">
-        By <span className="text-matrix-normal">Daniel Pardo </span>with ❤️
-      </h1>
+    <div
+      className={
+        "flex justify-between md:mx-20 my-5 md:my-10 px-5 md:px-10 text-md md:text-2xl " +
+        cormorantUpright.className
+      }
+    >
+      {localeData.locale == LocaleEnum.ES ? (
+        <h1 className="leading-tight text-matrix-light">
+          Creado con <span className="text-matrix-normal">Next.js</span>
+        </h1>
+      ) : (
+        <h1 className="leading-tight text-matrix-light">
+          Created with <span className="text-matrix-normal">Next.js</span>
+        </h1>
+      )}
+      {localeData.locale == LocaleEnum.ES ? (
+        <h1 className="leading-tight text-matrix-light">
+          Por <span className="text-matrix-normal">Daniel Pardo </span> con ❤️
+        </h1>
+      ) : (
+        <h1 className="leading-tight text-matrix-light">
+          By <span className="text-matrix-normal">Daniel Pardo </span>with ❤️
+        </h1>
+      )}
     </div>
   );
 }
